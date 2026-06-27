27c59e06a593db9b6df7e4a14c7bf3f96138c514
GURJOT SINGH <signingames115@gmail.com>
2026-06-27 19:33:39 +0530

    Time: 0 ms (100%), Space: 53.2 MB (85.05%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if(n==0) return 1.0;
+
+    return n<0? 1/temp(x,n) : temp(x,n);
+};
+
+    let temp = function(x,n){
+        if(n==0) return 1;
+
+        let ans = temp(x,parseInt(n/2));
+        
+        if(n%2==0) return ans*ans;
+        return ans*ans*x;
+    }
