f4dbd5c83b390d91786f3ba93d425508c2f511fd
GURJOT SINGH <signingames115@gmail.com>
2026-07-01 14:00:56 +0530

    Time: 35 ms (67.7%), Space: 73 MB (34.44%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let ans=[];
+    nums.sort((a,b)=>a-b);
+    for(let i=0;i<nums.length-2;i++){
+        if(i!=0 && nums[i-1] == nums[i]){
+            continue;
+        }
+        let j=i+1
+        let k= nums.length-1
+        while(j<k){
+            let sum= nums[i]+nums[j]+nums[k]
+            if(sum==0){
+                let cur = [nums[i], nums[j++],nums[k--]]
+                ans.push(cur)
+                while(j<k && nums[j-1] == nums[j]){
+                    j++
+                }
+                while(j<k && nums[k+1] == nums[k]){
+                    k--
+                }
+                }else if(sum<0){
+                    j++
+                }else{
+                    k--
+                }
+        }
+    }
+    return ans;
+};
