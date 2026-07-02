4b5f5c33f9ce780a16a58af3d6f6cc32a7520272
GURJOT SINGH <signingames115@gmail.com>
2026-07-02 12:24:35 +0530

    Time: 17 ms (18%), Space: 57.6 MB (31.08%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let arr = nums.sort((a,b)=>a-b);
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]!=i){
+            return i
+        }
+    }
+    return arr.length
+};
