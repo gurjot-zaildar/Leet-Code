5e666fb76c8c620c9be598ba75e3e1f35ab7041d
GURJOT SINGH <signingames115@gmail.com>
2026-07-19 12:19:33 +0530

    Time: 4 ms (53.32%), Space: 55.8 MB (22.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -8,8 +8,8 @@ var twoSum = function(nums, target) {
     let ans = [-1,-1];
     for(let i=0;i<nums.length;i++){
         if(map.has(target-nums[i])){
-            ans[0]=i;
-            ans[1]=map.get(target-nums[i]);
+            ans[1]=i;
+            ans[0]=map.get(target-nums[i]);
             return ans;
         }else{
             map.set(nums[i],i)
